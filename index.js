0da4e264e146ed8d556cc84dc6cf74eca221d259
DLF414 <Dmitry220198@gmail.com>
2017-09-18 00:18:04 +0300

    change to personal greetings

--- index.js
+++ index.js
@@ -2,3 +2,7 @@ console.log('Hello World');
 const name = process.argv[2];
 
 console.log(`Hi ${name}!`);
+const args = process.argv.slice(2);
+args.forEach((val,index)=>{
+        console.log(val);
+    })
