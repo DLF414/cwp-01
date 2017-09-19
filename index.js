87bbec29afc8320a26d7fda4dea94c488df632e5
DLF414 <Dmitry220198@gmail.com>
2017-09-20 00:33:54 +0300

    task5 final

--- index.js
+++ index.js
@@ -1,78 +1,132 @@
 const path = require('path');
 const fs = require('fs');
 
-const DirPath = process.argv[2];
-let summaryScript_code;
+const DIR_PATH = process.argv[2];
+const EXTNAME_TXT = '.txt';
+
 let copyright;
+let summaryScript_code =
+    'const fs = require(\'fs\');\n' +
+    'const path = require(\'path\');\n' +
+    '\n' +
+    '(function getFiles(baseDir) {\n' +
+    '    fs.readdir(baseDir, function (err, files){\n' +
+    '        for (let i in files) {\n' +
+    '            let currentDir = baseDir + path.sep + files[i];\n' +
+    '            fs.stat(currentDir, (err, stats) => {\n' +
+    '                    if (stats.isDirectory()) {\n' +
+    '                        getFiles(currentDir);\n' +
+    '                    } else {\n' +
+    '                        console.log(path.relative(__dirname, currentDir));\n' +
+    '                    }\n' +
+    '                }\n' +
+    '            );\n' +
+    '        }\n' +
+    '    });\n' +
+    '})(__dirname, null);';
 
-(function (){
-        let fl=true;
-        fs.access(DirPath,(err)=>
-        {
-            if(err) {
+
+(() => {
+    let fl = true;
+    fs.access(DIR_PATH, (err) => {
+            if (err) {
                 console.log(err);
-
-                console.log("Path error");
-                fl = false;
+                console.log("Ошибка пути");
             }
-        })
+            else {
 
-        if(fl){
-            let filesDir=createFilesDirectory();
-            createSummaryScriptCode();
-            createSummaryScriptFile();
-            setCopyright()
+                let TxtFilesDir = createTxtFilesDirectory();
+                createSummaryScriptFile();
+                setCopyright();
+                createTxtFilesCopy(DIR_PATH, TxtFilesDir);
+                whatchDir(DIR_PATH);
+            }
         }
-    }
-)();
+    )
 
-function setCopyright()
-{
+})();
+
+function setCopyright() {
     fs.readFile("copyright.json", (err, data) => {
-        if (err)
-        {
-            console.error("Copyright file error");
+        if (err) {
+            console.error("Ошибка при чтении файла конфигурации");
+            copyright = 'undefinded';
         }
-        else
-        {
+        else {
             copyright = JSON.parse(data);
         }
     })
 }
-function createFilesDirectory()
-{
-    let dirPath = `${DirPath}\\${path.basename(DirPath)}`;
-    fs.mkdir(dirPath,(err)=>{
-        if(err) {
-            console.log("Make dir error");
+
+function createTxtFilesDirectory() {
+    let dirPath = `${DIR_PATH}\\${path.basename(DIR_PATH)}`;
+    fs.mkdir(dirPath, (err) => {
+        if (err) {
+            console.log("Произошла ошибка при создании директория");
+            throw err;
         }
     });
-    return path;
+    return dirPath;
 }
-
 
 function createSummaryScriptFile() {
 
-    fs.appendFile(`${DirPath}\\summary.js`, summaryScript_code, (err) => {
-        console.log('The data was added to file!');
+    fs.appendFile(`${DIR_PATH}\\summary.js`, summaryScript_code, (err) => {
+        if (err) {
+            console.log(err);
+            console.log('Не удалось создать скрипт summary.js');
+        }
     });
 }
 
-function createSummaryScriptCode(){
-    summaryScript_code =
-        'const fs = require(\'fs\');\n' +
-        'const path = require(\'path\');\n' +
-        '\n' +
-        'function getFiles (dir){\n' +
-        '\n' +
-        '    let files = fs.readdirSync(dir);\n' +
-        '    for (let i in files){\n' +
-        '        let name = dir + path.sep + files[i];\n' +
-        '        if (fs.statSync(name).isDirectory()){\n' +
-        '            getFiles(name);\n' +
-        '        } else {\n' +
-        '            console.log(files[i]);\n' +
-        '        }\n' +
-        '    }\n' +
-        '}'
+function createTxtFilesCopy(dir, TxtFilesDir) {
+    fs.readdir(dir, function (err, files) {
+        if (err)
+            console.log(err);
+        else {
+            for (let file in files) {
+
+                let CurrentDirOrFile = `${dir}\\${files[file]}`;                              //путь для просмотра
+                if (fs.statSync(CurrentDirOrFile).isDirectory()) {
+                    createTxtFilesCopy(CurrentDirOrFile, TxtFilesDir);
+                } else {
+
+                    if (path.extname(CurrentDirOrFile) === EXTNAME_TXT) {
+                        fs.readFile(CurrentDirOrFile, 'utf8', (err, data) => {
+                            if (err) {
+                                console.log(`Невозможно прочитать файл: ${CurrentDirOrFile}`);
+                            }
+                            else {
+                                writeDataToTxt(TxtFilesDir + path.sep + files[file], data);
+
+                            }
+
+                        });
+                    }
+                }
+            }
+
+        }
+    });
+
+}
+
+function writeDataToTxt(filePath, data) {
+
+    let TextToAppend = copyright["copyright"] + data + copyright["copyright"];
+    fs.appendFile(filePath, TextToAppend, 'utf8', (err) => {
+        if (err) {
+            console.log(err);
+            console.log("Не удалось записать в файл");
+        }
+    });
+}
+
+function whatchDir(dirForTxt) {
+    fs.watch(dirForTxt, (eventType, filename) => {
+        if (filename) {
+            console.log(filename);
+
+        }
+    });
 }
