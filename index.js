4b28aab5af9e82e832d054d4d702c06ca0d4f7ef
DLF414 <Dmitry220198@gmail.com>
2017-09-19 22:27:30 +0300

    task5 start

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
-console.log('Hello World');
-const name = process.argv[2];
-
-console.log(`Hi ${name}!`);
-const args = process.argv.slice(2);
-args.forEach((val,index)=>{
-        console.log(val);
-    })
+const DirPath = process.argv[2];
+const path = require('path');
+const fs = require('fs');
+let summaryScript_code = null;
+fs.access(DirPath,(err) => {
+    console.log(err ? 'no access!' : 'can read/write');
+});
+function createSummaryScript() {
+    let FinallyScriptPath = DirPath + path.sep + 'summary.js';
+    fs.appendFile(FinallyScriptPath, summaryScript_code, (err) => {
+        if(err) throw err;
+        console.log('data was added to file!');
+    });
+}
+function createSummaryScriptCode(){
+    return ;
+}
+createSummaryScript()
